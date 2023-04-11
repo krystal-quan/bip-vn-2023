af5752f0d3d0f5df5c4a190f40ead2ab3b898e3d
CuongCevenn <21020118@vnu.edu.vn>
2023-04-11 22:56:19 +0700

    Fetch information

---
+++ player.py
@@ -0,0 +1,142 @@
+# Class Player này hiện đang có 18 biến, tương ứng với 18 cột trong bảng cleaned_player.
+# Các biến đều có hàm setter, getter.
+# Lưu ý hàm init hiện đang nhận 18 giá trị đầu vào để có thể nhận thông tin 
+# từ bảng cleaned_player kia, không nên sửa đầu vào hàm init nếu không sẽ lỗi.
+
+class Player:
+    def __init__(self, first_name, second_name, goals_scored, assists, total_points, 
+                 minutes, goals_conceded, creativity, influence, threat, bonus, bps, 
+                 ict_index, clean_sheets, red_cards, yellow_cards, selected_by_percent, 
+                 value):
+        self._first_name = first_name
+        self._second_name = second_name
+        self._goals_scored = goals_scored
+        self._assists = assists
+        self._total_points = total_points
+        self._minutes = minutes
+        self._goals_conceded = goals_conceded
+        self._creativity = creativity
+        self._influence = influence
+        self._threat = threat
+        self._bonus = bonus
+        self._bps = bps
+        self._ict_index = ict_index
+        self._clean_sheets = clean_sheets
+        self._red_cards = red_cards
+        self._yellow_cards = yellow_cards
+        self._selected_by_percent = selected_by_percent
+        self._value = value
+
+    def get_first_name(self):
+        return self._first_name
+
+    def set_first_name(self, first_name):
+        self._first_name = first_name
+
+    def get_second_name(self):
+        return self._second_name
+
+    def set_second_name(self, second_name):
+        self._second_name = second_name
+
+    def get_goals_scored(self):
+        return self._goals_scored
+
+    def set_goals_scored(self, goals_scored):
+        self._goals_scored = goals_scored
+
+    def get_assists(self):
+        return self._assists
+
+    def set_assists(self, assists):
+        self._assists = assists
+
+    def get_total_points(self):
+        return self._total_points
+
+    def set_total_points(self, total_points):
+        self._total_points = total_points
+
+    def get_minutes(self):
+        return self._minutes
+
+    def set_minutes(self, minutes):
+        self._minutes = minutes
+
+    def get_goals_conceded(self):
+        return self._goals_conceded
+
+    def set_goals_conceded(self, goals_conceded):
+        self._goals_conceded = goals_conceded
+
+    def get_creativity(self):
+        return self._creativity
+
+    def set_creativity(self, creativity):
+        self._creativity = creativity
+
+    def get_influence(self):
+        return self._influence
+
+    def set_influence(self, influence):
+        self._influence = influence
+
+    def get_threat(self):
+        return self._threat
+
+    def set_threat(self, threat):
+        self._threat = threat
+
+    def get_bonus(self):
+        return self._bonus
+
+    def set_bonus(self, bonus):
+        self._bonus = bonus
+
+    def get_bps(self):
+        return self._bps
+
+    def set_bps(self, bps):
+        self._bps = bps
+
+    def get_ict_index(self):
+        return self._ict_index
+
+    def set_ict_index(self, ict_index):
+        self._ict_index = ict_index
+
+    def get_clean_sheets(self):
+        return self._clean_sheets
+
+    def set_clean_sheets(self, clean_sheets):
+        self._clean_sheets = clean_sheets
+
+    def get_red_cards(self):
+        return self._red_cards
+
+    def set_red_cards(self, red_cards):
+        self._red_cards = red_cards
+
+    def get_yellow_cards(self):
+        return self._yellow_cards
+
+    def set_yellow_cards(self, yellow_cards):
+        self._yellow_cards = yellow_cards
+
+    def get_selected_by_percent(self):
+        return self._selected_by_percent
+
+    def set_selected_by_percent(self, selected_by_percent):
+        self._selected_by_percent = selected_by_percent
+
+    def get_value(self):
+        return self._value
+
+    def set_value(self, value):
+        self._value = value
+
+    def print_all(self):
+        print (str(self._first_name) + " " + str(self._second_name) + " " + str(self._goals_scored) + " " + str(self._assists) + " " + str(self._total_points) + " " + str(self._minutes) + " " + str(self._goals_conceded) + " " + str(self._creativity) + " " + str(self._influence) + " " + str(self._threat) + " " + str(self._bonus) + " " + str(self._bps) + " " + str(self._ict_index) + " " + str(self._clean_sheets) + " " + str(self._red_cards) + " " + str(self._yellow_cards) + " " + str(self._selected_by_percent) + " " + str(self._value))
+        return 0
+
+
---
+++ update_player_data.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import player
+
+# Đọc file cleaned_player.csv
+cleaned_player = pd.read_csv('cleaned_player.csv')
+
+# Hàm được tạo để có thể gọi từ file .py khác như main.py chẳng hạn.
+# Đầu vào là 1 player_list tạo sẵn hoặc không có gì cũng được.
+# Lưu ý nếu player_list tạo sẵn thì số phần tử trong List cũng phải >= số cầu thủ (683)
+# nếu không sẽ lỗi do đoạn code dưới chỉ gán giá trị chứ không tạo mới.
+# Do vậy t nghĩ đoạn code này nên được chạy đầu tiên trước khi tạo variable, parameter
+def set_player_list (player_list = None):
+    # Nếu đầu vào không có gì thì player_list sẽ được tạo.
+    if (player_list is None):
+        # Ở đây tự khởi tạo số lượng object trong list tương đương với số cầu thủ.
+        player_list = [None] * cleaned_player.shape[0]
+
+    # Duyệt thông tin theo hàng.
+    for index, row in cleaned_player.iterrows():
+        # Tạo mới player, nên hàm init mới phải đúng đầu vào (18 cái).
+        player_list[index] = player.Player(row[0], row[1], row[2], row[3], 
+                                           row[4], row[5], row[6], row[7], 
+                                           row[8], row[9], row[10], row[11], 
+                                           row[12], row[13], row[14], row[15], 
+                                           row[16], row[17])
+    # Trả lại giá trị List để sử dụng.
+    return player_list
+
+# Test code.
+if __name__ == "__main__":
+    player_list_con = [None] * 683
+    player_list = set_player_list(player_list_con)
+    player_list[0].print_all()
