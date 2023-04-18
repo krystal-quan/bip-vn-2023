95494f6af2666013f8edbd76423c060f0f97bb27
quanandon <tranngocanhquan03@gmail.com>
2023-04-18 18:44:45 +0700

    Update, change come files

--- parameter.py
+++ parameter.py
@@ -1,25 +1,36 @@
-#Parameters:
+import update_player_data as upd
 
+#Constraints, which are unchanged throughout the optimization process.
+E_CONSTANT = 1e-5
+K_CONSTANT = [] #Constants such that l << ✏ for all l 2 L, and 1 > 2 >...> L.
+GOALKEEPER_SELECTED = 2 #Number of goalkeepers required in the selected squad.
+DEFENDER_SELECTED = 5 #Number of defenders required in the selected squad.
+MIDFIELDER_SELECTED = 5 #Number of midfielders required in the selected squad.
+FORWARD_SELECTED = 3 #Number of forwards required in the selected squad.
+# Temporary not used, may use later.
+# maxSameTeamSelected = 3 #Maximum number of players allowed in the selected squad from the same team.
+PLAYER_REQUIRED = 11 #Number of players required in the starting line-up.
+GOALKEEPER_REQUIRED = 1 #Number of goalkeepers required in the starting line-up.
+DEFENDER_REQUIRED = 3 #Minimum number of defenders required in the starting line-up.
+MIDFIELDER_REQUIRED = 3 #Minimum number of midfielders required in the starting line-up.
+FORWARD_REQUIRED = 1 #Minimum number of forwards required in the starting line-up.
+START_BUDGET = 100 #Starting budget.
+BETA = 1e5 #Sufficiently high constant.
+ALPHA = 1e5 #Sufficiently high constant.
+SUB_PLAYER = 4 #Number of players which are substitutes.
+SUB_GOALKEEPER = 1 #Number of goalkeepers which are substitutes.
+FREE_TOKEN = 1 #Number of free transfers given every gameweek.
+MAX_TOKEN = 2 #Maximum number of free transfers possible to accumulate over gameweeks.
+
+#Variables, which are changed throughout the optimization process.
 point = []
-eConstant = 1e-5
-kconstant = [] #Constants such that l << ✏ for all l 2 L, and 1 > 2 >...> L.
-sellPrice = [] #Sell price of player p in a gameweek t.
-playerValue = [] #Value of player p in a gameweek t.
-deductedPoint = 0 #Number of points deducted for each penalized transfer.
-goalkeeperSelected = 2 #Number of goalkeepers required in the selected squad.
-defenderSelected = 5 #Number of defenders required in the selected squad.
-midfielderSelected = 5 #Number of midfielders required in the selected squad.
-forwardSelected = 3 #Number of forwards required in the selected squad.
-maxSameTeamSelected = 3 #Maximum number of players allowed in the selected squad from the same team.
-playerRequired = 11 #Number of players required in the starting line-up.
-goalkeeperRequired = 1 #Number of goalkeepers required in the starting line-up.
-defenderRequired = 3 #Minimum number of defenders required in the starting line-up.
-midfielderRequired = 3 #Minimum number of midfielders required in the starting line-up.
-forwardRequired = 1 #Minimum number of forwards required in the starting line-up.
-startBudget = 100000000 #Starting budget.
-beta = 1e5 #Sufficiently high constant.
-alpha = 1e5 #Sufficiently high constant.
-subPlayer = 4 #Number of players which are substitutes.
-subGoal = 1 #Number of goalkeepers which are substitutes.
-_q = 1 #Number of free transfers given every gameweek.
-q_ = 2 #Maximum number of free transfers possible to accumulate over gameweeks.
+# May delete this variable.
+# sellPrice = [] #Sell price of player p in a gameweek t.
+# playerValue = [] #Value of player p in a gameweek t.
+deductedPoint = [] #Number of points deducted for each penalized transfer.
+player = upd.set_player_list()
+
+# Test Code
+for _ in player:
+    print(_)
+
--- player.py
+++ player.py
@@ -4,6 +4,8 @@
 # từ bảng cleaned_player kia, không nên sửa đầu vào hàm init nếu không sẽ lỗi.
 
 class Player:
+    global total_player
+    total_player = 0
     def __init__(self, first_name, second_name, goals_scored, assists, total_points, 
                  minutes, goals_conceded, creativity, influence, threat, bonus, bps, 
                  ict_index, clean_sheets, red_cards, yellow_cards, selected_by_percent, 
@@ -26,6 +28,7 @@ class Player:
         self._yellow_cards = yellow_cards
         self._selected_by_percent = selected_by_percent
         self._value = value
+        self._id = 1
 
     def get_first_name(self):
         return self._first_name
@@ -136,7 +139,14 @@ class Player:
         self._value = value
 
     def print_all(self):
-        print (str(self._first_name) + " " + str(self._second_name) + " " + str(self._goals_scored) + " " + str(self._assists) + " " + str(self._total_points) + " " + str(self._minutes) + " " + str(self._goals_conceded) + " " + str(self._creativity) + " " + str(self._influence) + " " + str(self._threat) + " " + str(self._bonus) + " " + str(self._bps) + " " + str(self._ict_index) + " " + str(self._clean_sheets) + " " + str(self._red_cards) + " " + str(self._yellow_cards) + " " + str(self._selected_by_percent) + " " + str(self._value))
+        print(
+            f"{str(self._first_name)} {str(self._second_name)} {str(self._goals_scored)} {str(self._assists)} {str(self._total_points)} {str(self._minutes)} {str(self._goals_conceded)} {str(self._creativity)} {str(self._influence)} {str(self._threat)} {str(self._bonus)} {str(self._bps)} {str(self._ict_index)} {str(self._clean_sheets)} {str(self._red_cards)} {str(self._yellow_cards)} {str(self._selected_by_percent)} {str(self._value)}"
+        )
         return 0
+    
+    def __str__(self):
+        return f"Player {self._id}: {self._first_name} {self._second_name} - Value: {self._value}"
+    
+    
 
 
