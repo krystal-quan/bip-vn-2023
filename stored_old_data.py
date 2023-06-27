74588a2266e278279a32b6d5c7f63488e4db3c69
Cuong-21020118 <100205153+CuongCevenn@users.noreply.github.com>
2023-06-27 21:36:56 +0700

    Update stored_old_data.py

--- stored_old_data.py
+++ stored_old_data.py
@@ -4,15 +4,26 @@ import copy
 import update_player_data as upd
 import parameter as prm
 
+# A numpy object, which contain all old data of player in old week 
+# (When program is processing data of week 20, player_data stored data from week 1 to week 19)
 player_data = np.empty((prm.TOTAL_PLAYERS,38), dtype=player.Player)
 
+'''
+Input:
+    player is an object stored data of a Player
+    x is id of this player
+    y is week - 1 (Because order of table is from 0 and order of week is from 1)
+Output: None
+To do: Store old data to player_data for reuse.
+'''
 def set_player_data(player, x, y):
     player_data[x, y] = copy.deepcopy(player)
 
+'''
+Input: None
+Output: None
+To do: Get player_data.
+'''
 def get_player_data():
     return player_data
-
-def print_player_data():
-    for i in range(25):
-        player_data[i, 0].print_all()
         
