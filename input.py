4fbbacc5d9f577d33c89013c69012b1fd920d236
21020127HaCongNga <100206966+HaCongNga@users.noreply.github.com>
2023-07-08 13:58:27 +0700

    Update input.py

--- input.py
+++ input.py
@@ -7,7 +7,7 @@ import chosen_team as cht
 prm.playerList = upd.set_player_list()
 
 # transfer: number transfer left in file input
-global transfer
+
 
 """
 Input: .json file contain data of main roster and transfer, file must be correct format.
@@ -15,68 +15,36 @@ Output: player_list is a list of dictionary, each dictionary has name and positi
 TO DO: change data in .json file to a list in Python
 """
 def extractJsonFileToPython(filename):
-    global transfer
+   
     with open(filename) as f:
         data = json.load(f)
     
     main_roster = data['Main Roster']
-    transfer = data['transfer']
+    prm.transferLeft = data['transfer']
     prm.gameWeek = data['Week']
     prm.startWeek = data['Week']
-    main_players = data['Players chosen']
+    #main_players = data['Players chosen']
 
     # Store main_roster
     player_list = []
     player_id_list = []
 
     # Store main_player
-    main_player_list = []
-    main_player_id_list = []
-
-    captain_id = 0
-    vice_id = 0
-
     # Append in the main_roster list
     for player_data in main_roster:
         player_name = player_data['player']
         player_position = player_data['position']
-        captain_value = player_data['Captain']
-        vice_value = player_data['Vice captain']
         player_list.append({'name' : player_name, 'position' : player_position})
         id = compare.continuous_search(prm.playerList,player_name, 0, len(prm.playerList)) + 1
         player_id_list.append(id)
-        captain_value = player_data['Captain']
-        vice_value = player_data['Vice captain']
-        if captain_value == 1 :
-            captain_id = id
-        if vice_value == 1 :
-            vice_id = id
+        
     
-    # Append in the main_player list
-    for main_player in main_players :
-        player_name = main_player['player']
-        player_position = main_player['position']
-        main_player_list.append({'name' : player_name, 'position' : player_position})
-        id = compare.continuous_search(prm.playerList,player_name, 0, len(prm.playerList)) + 1
-        main_player_id_list.append(id)
+    return player_id_list
 
-
-
-    return captain_id, vice_id, player_id_list, main_player_id_list, transfer
-
+# Get input from requirement and update the player IDs chosen. 
 def getInput():
     filename = 'input_full.json'
-    captain_id, vice_id, player_id, mainPlayer_id, transferLeft = extractJsonFileToPython(filename)
-    #print(player_id)
-    #print(mainPlayer_id)
-    #print(transferLeft)
-    """
-    for player in players:
-        print(f"Player: {player['name']}, Position: {player['position']}")
-        print(compare.continuous_search(prm.playerList,player['name'], 0, len(prm.playerList)) + 1)
-    """
-   # prm.transferLeft = transferLeft
-    #print(prm.transferLeft)
-    prm.chosenPlayerList.append(cht.Chosen_Team(captain_id, vice_id, player_id, mainPlayer_id, prm.transferLeft, prm.point, 0, 0))
-    #print(prm.chosenPlayerList[0].printPlayerList())
-
+    player_id = extractJsonFileToPython(filename)
+    prm.chosenPlayerList.append(cht.Chosen_Team(player_id[0], player_id[1], player_id, player_id[0:12], prm.transferLeft, prm.point, [40], [player_id[0]]))
+    prm.playerInput = player_id
+    
