bc39f1c4f100bf80269df3dfd1b90673f3c27400
21020127HaCongNga <100206966+HaCongNga@users.noreply.github.com>
2023-07-08 09:05:57 +0700

    Update processing.py

--- processing.py
+++ processing.py
@@ -11,18 +11,22 @@ def printPlayerList():
         print(player)
 
 def completeWeek(gameWeek):
+    #print(f"gameweek in prc : {gameWeek}")
     tempPoint = 0
     cap_available = True
-    cap_id = prm.chosenPlayerList[gameWeek - 1].captain
-    print(f"cap_id + {cap_id}")
+    cap_id = prm.chosenPlayerList[gameWeek - prm.startWeek].captain
+    #if (cap_id == None) :
+    #    cap_id = prm.chosenPlayerList[gameWeek-1].mainTeam[0]
+    #print(f"cap_id in processing: {cap_id}")
+    #print(f"cap_id + {cap_id}")
     if prm.playerList[cap_id]._total_points != -1:
         cap_available = True
-    for i in prm.chosenPlayerList[gameWeek-1].mainTeam:
+    for i in prm.chosenPlayerList[gameWeek - prm.startWeek].mainTeam:
         if prm.playerList[i]._total_points != -1:
-            if (i == prm.chosenPlayerList[gameWeek-1].captain and cap_available):
+            if (i == prm.chosenPlayerList[gameWeek - prm.startWeek].captain and cap_available):
                 tempPoint += prm.playerList[i]._total_points * 2
             elif (
-                i == prm.chosenPlayerList[gameWeek - 1].vice_captain
+                i == prm.chosenPlayerList[gameWeek - prm.startWeek].vice_captain
                 and not cap_available
             ):
                 tempPoint += prm.playerList[i]._total_points * 2
@@ -31,4 +35,6 @@ def completeWeek(gameWeek):
     prm.point += tempPoint
     prm.transferLeft = min(prm.transferLeft + prm.FREE_TOKEN, prm.MAX_TOKEN)
     
+            
+
             
