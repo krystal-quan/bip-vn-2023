72c0e6b46f58b4223c77ae23107e5947a17b1bba
21020127HaCongNga <100206966+HaCongNga@users.noreply.github.com>
2023-07-08 09:06:33 +0700

    Update update_player_data.py

--- update_player_data.py
+++ update_player_data.py
@@ -9,17 +9,25 @@ Output:
     playerList<list> is a list of Player, each Player data is up-to-date for the file 'cleaned_player.csv'
 To do: Make or set data for playerList
 '''
+
 def set_player_list (playerList = None):
+    # Đọc file cleaned_player.csv
     cleaned_player = pd.read_csv('cleaned_player.csv')
+    
     if (playerList is None):
+       
         playerList = [None] * cleaned_player.shape[0]
 
+    
     for index, row in cleaned_player.iterrows():
+        # Tạo mới player, nên hàm init mới phải đúng đầu vào (18 cái).
         playerList[index] = plr.Player(index + 1, row[0], row[1], row[2], row[3], 
                                            row[4], row[5], row[6], row[7], 
                                            row[8], row[9], row[10], row[11], 
                                            row[12], row[13], row[14], row[15], 
-                                           row[16], row[17], row[18])
+                                           row[16], row[17], row[18], 0)
     prm.TOTAL_PLAYERS = len(playerList)
+    
+    
     return playerList
     
